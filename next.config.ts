2aa5271f3950564598c62a4561b0cccfa117e9c1
Stelannesse <stelannesse@github.com>
2026-07-19 22:26:55 +0200

    .voir si image blanche disparait

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploads.mangadex.org', // Pour les images MangaDex
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org', // Pour les images de films/séries (si tu utilises TMDB)
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.myanimelist.net', // Pour les images d'Animes
+      },
+    ],
+  },
 };
 
 export default nextConfig;
